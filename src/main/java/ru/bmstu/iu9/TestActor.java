0fd56ff00cfe304b90d505b591763c3d3248c0af
DionisSerbin <reaganusm@gmail.com>
2021-11-19 19:30:05 +0300

    Scripted auto-commit on change (2021-11-19 19:30:05) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/TestActor.java
+++ src/main/java/ru/bmstu/iu9/TestActor.java
@@ -4,18 +4,28 @@ import akka.actor.AbstractActor;
 import akka.actor.ActorSelection;
 import akka.japi.pf.ReceiveBuilder;
 
+import java.util.ArrayList;
+
 public class TestActor extends AbstractActor {
 
-    private ActorSelection store = getContext().actorSelection()
+    private ActorSelection store = getContext().actorSelection("/user/storeActor");
+
+    private ArrayList<Test> 
 
     @Override
     public Receive createReceive() {
         return ReceiveBuilder.create().
                 match(TestMessage.class, m -> {
+                    store.
+                            tell(
+                                    new MessageStor(
+                                            m.getPackageId(),
 
+                                    )
+                            );
                 }
                 )
-                
+
     }
 
 }
