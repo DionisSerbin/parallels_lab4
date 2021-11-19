13a771c890dac72eebce2a73a431a6a25e127c4a
DionisSerbin <reaganusm@gmail.com>
2021-11-19 18:18:02 +0300

    Scripted auto-commit on change (2021-11-19 18:18:02) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/AkkaApp.java
+++ src/main/java/ru/bmstu/iu9/AkkaApp.java
@@ -3,7 +3,9 @@ package ru.bmstu.iu9;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
+import akka.http.javadsl.Http;
 import akka.http.javadsl.server.AllDirectives;
+import akka.stream.ActorMaterializer;
 
 import java.io.IOException;
 
@@ -11,6 +13,8 @@ public class AkkaApp extends AllDirectives {
 
     public static void main(String[] args) throws IOException {
         final ActorSystem system = ActorSystem.create("lab4");
+        final Http http = Http.get(system);
+        final ActorMaterializer materializer = ActorMaterializer.create(system);
         ActorRef router = system.actorOf(Props.create(ActorRouter.class));
     }
 }
