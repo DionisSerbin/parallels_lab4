253e975ba19d9e4c9f19153d8079df632264b493
DionisSerbin <reaganusm@gmail.com>
2021-11-19 19:13:49 +0300

    Scripted auto-commit on change (2021-11-19 19:13:49) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/TestPackageActor.java
+++ src/main/java/ru/bmstu/iu9/TestPackageActor.java
@@ -1,16 +1,20 @@
 package ru.bmstu.iu9;
 
 import akka.actor.AbstractActor;
+import akka.actor.ActorSelection;
 import akka.japi.pf.ReceiveBuilder;
 
 public class TestPackageActor extends AbstractActor {
+
+    private ActorSelection testPerformerRouter = getContext().
+
     @Override
     public Receive createReceive() {
         return ReceiveBuilder.create().
                 match(
                         MassageTestPackage.class, m -> {
                             for (Test test : m.getTests()) {
-                                te
+                                testPer
                             }
                         }
                 )
