d6f370a9bef791ac5ba9c0a2a3bd59402b440717
DionisSerbin <reaganusm@gmail.com>
2021-11-19 19:32:04 +0300

    Scripted auto-commit on change (2021-11-19 19:32:04) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/TestActor.java
+++ src/main/java/ru/bmstu/iu9/TestActor.java
@@ -4,13 +4,20 @@ import akka.actor.AbstractActor;
 import akka.actor.ActorSelection;
 import akka.japi.pf.ReceiveBuilder;
 
+import javax.script.ScriptEngineManager;
 import java.util.ArrayList;
 
 public class TestActor extends AbstractActor {
 
     private ActorSelection store = getContext().actorSelection("/user/storeActor");
+    private final String SCRIPT = "nashorn";
 
-    private ArrayList<Test> buildTest()
+    private ArrayList<Test> buildTest(String jsScript, String functionName, String testName,
+                                      String expectedResult, ArrayList<Integer> params){
+
+        ScriptEngine scriptEngine = new ScriptEngineManager().getEngineByName()
+
+    }
 
     @Override
     public Receive createReceive() {
