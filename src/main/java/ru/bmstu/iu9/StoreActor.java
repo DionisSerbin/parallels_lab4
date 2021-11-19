fd345b520b900917e92343fa7485be64c7e626dd
DionisSerbin <reaganusm@gmail.com>
2021-11-19 18:24:32 +0300

    Scripted auto-commit on change (2021-11-19 18:24:32) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/StoreActor.java
+++ src/main/java/ru/bmstu/iu9/StoreActor.java
@@ -5,6 +5,6 @@ import akka.actor.AbstractActor;
 public class StoreActor extends AbstractActor {
     @Override
     public Receive createReceive() {
-        return null;
+        return ;
     }
 }
