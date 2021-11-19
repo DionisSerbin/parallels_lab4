0f6fdfc875f254e215b58c20d43333167f93c6a6
DionisSerbin <reaganusm@gmail.com>
2021-11-19 19:19:00 +0300

    Scripted auto-commit on change (2021-11-19 19:19:00) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/TestMessage.java
+++ src/main/java/ru/bmstu/iu9/TestMessage.java
@@ -4,5 +4,31 @@ public class TestMessage {
     private Integer packageId;
     private String jsScript;
     private String functionName;
-    private 
+    private Test test;
+
+    public TestMessage(Integer packageId, String jsScript,
+                       String functionName, Test test){
+
+        this.packageId = packageId;
+        this.functionName = functionName;
+        this.jsScript = jsScript;
+        this.test = test;
+
+    }
+
+    public Integer getPackageId() {
+        return this.packageId;
+    }
+
+    public String getJsScript() {
+        return this.jsScript;
+    }
+
+    public Test getTest() {
+        return this.test;
+    }
+
+    public String getFunctionName() {
+        return this.functionName;
+    }
 }
