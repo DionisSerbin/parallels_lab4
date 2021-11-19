72f9f380eb3ece9278b6de780c9d4bed1c9716b5
DionisSerbin <reaganusm@gmail.com>
2021-11-19 19:34:42 +0300

    Scripted auto-commit on change (2021-11-19 19:34:42) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/TestActor.java
+++ src/main/java/ru/bmstu/iu9/TestActor.java
@@ -4,7 +4,10 @@ import akka.actor.AbstractActor;
 import akka.actor.ActorSelection;
 import akka.japi.pf.ReceiveBuilder;
 
+import javax.script.Invocable;
+import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
 import java.util.ArrayList;
 
 public class TestActor extends AbstractActor {
@@ -13,10 +16,16 @@ public class TestActor extends AbstractActor {
     private final String SCRIPT = "nashorn";
 
     private ArrayList<Test> buildTest(String jsScript, String functionName, String testName,
-                                      String expectedResult, ArrayList<Integer> params){
+                                      String expectedResult, ArrayList<Integer> params) throws ScriptException, NoSuchMethodException {
 
-        ScriptEngine scriptEngine = new ScriptEngineManager().getEngineByName()
-
+        ScriptEngine scriptEngine = new ScriptEngineManager().getEngineByName(SCRIPT);
+        scriptEngine.eval(jsScript);
+        Invocable invocable = (Invocable) scriptEngine;
+        String res = invocable.invokeFunction(functionName, params.toArray()).toString();
+        Test test = new Test(testName, expectedResult,
+                params, expectedResult.equals(res));
+        ArrayList<Test> testsNow = new ArrayList<>();
+        test
     }
 
     @Override
