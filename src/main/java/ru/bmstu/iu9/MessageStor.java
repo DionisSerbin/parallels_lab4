3801b9554c202ab002a2641cf20a60cb42b7bd93
DionisSerbin <reaganusm@gmail.com>
2021-11-19 18:28:18 +0300

    Scripted auto-commit on change (2021-11-19 18:28:18) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/MessageStor.java
+++ src/main/java/ru/bmstu/iu9/MessageStor.java
@@ -1,4 +1,17 @@
 package ru.bmstu.iu9;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.ArrayList;
+
 public class MessageStor {
+    private final String ID_PACKAGE_STRING = "packageId";
+    private final String TEST_STRING = "test";
+
+    @JsonProperty(ID_PACKAGE_STRING)
+    private Integer packageId;
+
+    @JsonProperty(TEST_STRING)
+    private ArrayList<>
+
 }
