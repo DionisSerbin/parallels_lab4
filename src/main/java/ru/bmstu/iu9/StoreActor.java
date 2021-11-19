55157f0d9d3a5902f9c4c3a8aee94127e202d987
DionisSerbin <reaganusm@gmail.com>
2021-11-19 18:45:21 +0300

    Scripted auto-commit on change (2021-11-19 18:45:21) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/StoreActor.java
+++ src/main/java/ru/bmstu/iu9/StoreActor.java
@@ -3,10 +3,27 @@ package ru.bmstu.iu9;
 import akka.actor.AbstractActor;
 import akka.japi.pf.ReceiveBuilder;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class StoreActor extends AbstractActor {
+
+    private Map<Integer, ArrayList<Test>> store = new HashMap<>();
+
     @Override
     public Receive createReceive() {
         return ReceiveBuilder.create().
-                match();
+                match( MessageStor.class, m ->{
+                    if (store.containsKey(m.getPackageId())) {
+                        ArrayList<Test> testNow = store.get(m.getPackageId());
+                        testNow.addAll()
+                    }
+                }
+                );
+    }
+
+    public Map<Integer, ArrayList<Test>> getStore() {
+        return this.store;
     }
 }
