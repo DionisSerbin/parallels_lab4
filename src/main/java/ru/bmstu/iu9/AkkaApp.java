d1b1e25d92ac0ee33079fd637628d9ba7cd671be
DionisSerbin <reaganusm@gmail.com>
2021-11-19 19:43:10 +0300

    Scripted auto-commit on change (2021-11-19 19:43:10) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/AkkaApp.java
+++ src/main/java/ru/bmstu/iu9/AkkaApp.java
@@ -1,10 +1,14 @@
 package ru.bmstu.iu9;
 
+import akka.NotUsed;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
 import akka.http.javadsl.Http;
+import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.HttpResponse;
 import akka.http.javadsl.server.AllDirectives;
+import akka.http.javadsl.server.Route;
 import akka.routing.RoundRobinPool;
 import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
@@ -17,6 +21,15 @@ public class AkkaApp extends AllDirectives {
     private static final String TEST_PACKAGE_ACTOR = "testPackageActor";
     private static final String TEST_PERFORM_ACTOR = "testPerformActor";
     private static final Integer POOLS_NUMBER = 5;
+    private static final String ID_PACKAGE_STRING = "packageId";
+
+    private Route createRoute() {
+        return route(
+                get(
+                        () -> parameter(())
+                )
+        )
+    }
 
     public static void main(String[] args) throws IOException {
         final ActorSystem system = ActorSystem.create("lab4");
@@ -29,6 +42,10 @@ public class AkkaApp extends AllDirectives {
                 new RoundRobinPool(POOLS_NUMBER).
                         props(Props.create(TestActor.class)),
                 TEST_PERFORM_ACTOR);
-        final Flow
+        final Flow<
+                HttpRequest,
+                HttpResponse,
+                NotUsed
+                > route =
     }
 }
