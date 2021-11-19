93152b7964fe35e048645d1d1138fbf4daa5682e
DionisSerbin <reaganusm@gmail.com>
2021-11-19 19:05:25 +0300

    Scripted auto-commit on change (2021-11-19 19:05:25) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/MassageTestPackage.java
+++ src/main/java/ru/bmstu/iu9/MassageTestPackage.java
@@ -1,4 +1,15 @@
 package ru.bmstu.iu9;
 
-public class TestPackageMassage {
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.ArrayList;
+
+public class MassageTestPackage {
+    private final String ID_PACKAGE_STRING = "packageId";
+    
+    @JsonProperty(ID_PACKAGE_STRING)
+    private Integer packageId;
+
+    @JsonProperty(TEST_STRING)
+    private ArrayList<Test> test;
 }
