66d9ddc5837c2165c18f795c63ed53f9c201b48a
DionisSerbin <reaganusm@gmail.com>
2021-11-19 19:06:26 +0300

    Scripted auto-commit on change (2021-11-19 19:06:26) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/MassageTestPackage.java
+++ src/main/java/ru/bmstu/iu9/MassageTestPackage.java
@@ -6,10 +6,11 @@ import java.util.ArrayList;
 
 public class MassageTestPackage {
     private final String ID_PACKAGE_STRING = "packageId";
-    
+    private final String JS_SCRIPT_STRING = "jsScript";
+    private final String FUNCTION_NAME_STRING = "functionName";
+    private final String TESTS_STRING = "tests";
+
     @JsonProperty(ID_PACKAGE_STRING)
     private Integer packageId;
-
-    @JsonProperty(TEST_STRING)
-    private ArrayList<Test> test;
+    
 }
