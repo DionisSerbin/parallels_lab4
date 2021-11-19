176b13b87fce3f515322571e93a2babfe4be533f
DionisSerbin <reaganusm@gmail.com>
2021-11-19 18:31:15 +0300

    Scripted auto-commit on change (2021-11-19 18:31:14) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/Test.java
+++ src/main/java/ru/bmstu/iu9/Test.java
@@ -4,7 +4,12 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class Test {
 
-    private final 
+    private final String TEST_NAME_STRING = "testName";
+    private final String EXPECTED_RESULT_STRING = "expectedResult";
 
-    @JsonProperty()
+    @JsonProperty(TEST_NAME_STRING)
+    private String testName;
+
+    @JsonProperty(EXPECTED_RESULT_STRING)
+    private String expectedREsult;
 }
